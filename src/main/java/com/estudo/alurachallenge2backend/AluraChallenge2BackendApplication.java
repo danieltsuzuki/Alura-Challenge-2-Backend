7c7e7c5f8640e2657f2691fcf64f9708537ab944
danietsuzuki <massao.tsuzuki@gmail.com>
2023-05-17 21:53:39 -0300

    Criando banco de dados e tabelas de despesas e receitas

---
+++ src/main/java/com/estudo/alurachallenge2backend/AluraChallenge2BackendApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.alurachallenge2backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraChallenge2BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AluraChallenge2BackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/estudo/alurachallenge2backend/AluraChallenge2BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.alurachallenge2backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraChallenge2BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
